110ca02b7704d8da147f496135a8c086f11f5358
szaboferenc2022d-ai <szabo.ferenc.2022d@telekimezotur.hu>
2026-09-24 09:43:12 +0200

    fel kesz

--- CegApp/Alkalmazott.cs
+++ CegApp/Alkalmazott.cs
@@ -6,5 +6,23 @@ namespace CegApp
 {
     internal class Alkalmazott
     {
+        public string Nev { get;}
+        protected int Alapber { get; }
+
+        public Alkalmazott(string nev, int alapber)
+        {
+            Nev = nev;
+            Alapber = alapber;
+        }
+
+        public virtual int FizetesSzamitas()
+        {
+            return Alapber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev}, Fizetés: {FizetesSzamitas()} Ft";
+        } 
     }
 }
---
+++ CegApp/Menedzser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CegApp
+{
+    internal class Menedzser : Alkalmazott
+    {
+        public int Bonusz { get; }
+        public Menedzser(int bonusz) : base(nev, alapber)
+        {
+            Bonusz = bonusz;
+        }
+    }
+}
